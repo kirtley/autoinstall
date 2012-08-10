a4755a59b379e69eb552faf5ac13f13d724297cf
kirtley <kirtley@vnc3>
2012-08-10 12:15:11 +0000

    new file:   reset-vnc4.sh

---
+++ reset-vnc4.sh
@@ -0,0 +1,9 @@
+sudo rm /etc/init.d/vnc4server
+sudo userdel remoteuser
+sudo userdel remoteuser1
+sudo userdel remoteuser2
+sudo userdel remoteuser3
+sudo userdel remoteuser4
+sudo userdel remoteuser5
+sudo rm -r /home/remoteuser*
+
