e445932a4e3e4e3df0fd08cc81b244faaff0465e
kirtley <kirtley@GX520>
2012-08-09 22:50:51 -0700

    modified:   configure-guacamole.sh

--- configure-guacamole.sh
+++ configure-guacamole.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 read -p "What is the hostname of the VNC server?" vnchostname
 read -p "What is the IP of the VNC server?" vncip
 read -s -p "What is the VNC password?" vncpassword
@@ -10,7 +10,7 @@ echo ""
 for i in {1..5}
 do
 vncport="590"$i
-	sudo /bin/sh -c 'echo "<authorize username=\"$vnchostname$i\" password=\"$guacpassword$i\"\>" >> /etc/guacamole/user-mapping.xml'
+	sudo /bin/sh -c 'echo "<authorize username=\"$vnchostname$i\" password=\"$guacpassword$i\">" >> /etc/guacamole/user-mapping.xml'
 	sudo /bin/sh -c 'echo "     <protocol>vnc</protocol>" >> /etc/guacamole/user-mapping.xml'
 	sudo /bin/sh -c 'echo "     <param name=\"hostname\">$vncip</param>" >> /etc/guacamole/user-mapping.xml'
 	sudo /bin/sh -c 'echo "     <param name=\"port\">$vncport</param>" >> /etc/guacamole/user-mapping.xml'
