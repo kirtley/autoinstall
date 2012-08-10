5ea27b9ecf455ef034d6a3a7dae8f35a5a9217f0
kirtley <kirtley@GX520>
2012-08-09 23:11:54 -0700

    modified:   configure-guacamole.sh

--- configure-guacamole.sh
+++ configure-guacamole.sh
@@ -10,10 +10,10 @@ echo ""
 for i in {1..5}
 do
 vncport="590"$i
-	sudo echo "<authorize username=\"$vnchostname$i\" password=\"$guacpassword$i\">" >> /etc/guacamole/user-mapping.xml
-	sudo echo "     <protocol>vnc</protocol>" >> /etc/guacamole/user-mapping.xml
-	sudo echo "     <param name=\"hostname\">$vncip</param>" >> /etc/guacamole/user-mapping.xml
-	sudo echo "     <param name=\"port\">$vncport</param>" >> /etc/guacamole/user-mapping.xml
-	sudo echo "     <param name=\"password\">$vncpassword</param>" >> /etc/guacamole/user-mapping.xml
-	sudo echo "</authorize>" >> /etc/guacamole/user-mapping.xml
+	echo "<authorize username=\"$vnchostname$i\" password=\"$guacpassword$i\">" | sudo tee -a /etc/guacamole/user-mapping.xml
+	echo "     <protocol>vnc</protocol>" | sudo tee -a /etc/guacamole/user-mapping.xml
+	echo "     <param name=\"hostname\">$vncip</param>" | sudo tee -a /etc/guacamole/user-mapping.xml
+	echo "     <param name=\"port\">$vncport</param>" | sudo tee -a /etc/guacamole/user-mapping.xml
+	echo "     <param name=\"password\">$vncpassword</param>" | sudo tee -a /etc/guacamole/user-mapping.xml
+	echo "</authorize>" | sudo tee -a /etc/guacamole/user-mapping.xml
 done
