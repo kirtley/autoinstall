a4f03b8b787ed3de9d882da96b6a6c5cce529d14
kirtley <kirtley@GX520>
2012-08-13 22:40:02 -0700

    new file:   install-grub-customizer.sh

---
+++ install-grub-customizer.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+# Kirtley Wienbroer
+# kirtley@osgenie.com
+sudo add-apt install -y python-software-properties
+sudo add-apt-repository -y ppa:danielrichter2007/grub-customizer
+sudo apt-get update
+sudo apt-get install -y grub-customizer
