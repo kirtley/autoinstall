84d68eba26d1e113f66b70f0657758bf2a6d93ac
kirtley <kirtley@kirtley-zbox>
2012-08-13 18:31:32 -0700

    deleted:    configfiles/guacamole/user-mapping.xml 	deleted:    configfiles/guacamole/user-mapping1.xml 	deleted:    configfiles/guacamole/user-mapping2.xml 	deleted:    configfiles/refresh-git.sh 	deleted:    configfiles/status-git.sh 	deleted:    configfiles/virbr0-eth1 	deleted:    configfiles/vnc4server/xstartup 	deleted:    configure-guacamole-md5.sh 	deleted:    configure-guacamole.sh 	deleted:    configure-vnc4server-ubuntu-desktop.sh 	deleted:    configure-vnc4server-unity-2d.sh 	deleted:    configure-vnc4server.sh 	deleted:    reset-guac.sh 	deleted:    reset-vnc4.sh

--- reset-guac.sh
+++
@@ -1,4 +0,0 @@
-sudo rm /etc/guacamole/user-mapping.xml
-sudo touch /etc/guacamole/user-mapping.xml
-sudo chown root:guacamole-web /etc/guacamole/user-mapping.xml
-sudo chmod 640 /etc/guacamole/user-mapping.xml
--- reset-vnc4.sh
+++
@@ -1,9 +0,0 @@
-sudo rm /etc/init.d/vnc4server
-sudo userdel remoteuser
-sudo userdel remoteuser1
-sudo userdel remoteuser2
-sudo userdel remoteuser3
-sudo userdel remoteuser4
-sudo userdel remoteuser5
-sudo rm -r /home/remoteuser*
-
