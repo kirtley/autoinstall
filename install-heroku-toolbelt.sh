5fbdc6a7a245e8abd3355d9f7203eec409d69bc9
kirtley <kirtley@kirtley-zbox>
2012-11-15 19:49:11 -0800

    adding

---
+++ install-heroku-toolbelt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Kirtley Wienbroer
+# kirtley@osgenie.com
+sudo apt-get update
+sudo apt-get install -y rake ruby-bundler rails
+cd /tmp/
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku login
