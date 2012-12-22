b5dee13e3e634472c8483751432a7a27c89c202f
kirtley <kirtley@kirtley-three>
2012-12-22 16:14:38 -0500

    new file:   install-chrome-stable-RAM-cache.sh 	modified:   install-chrome-stable.sh

---
+++ install-chrome-stable-RAM-cache.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Kirtley Wienbroer
+# kirtley@osgenie.com
+
+##Install Chrome 
+echo "Adding Google Chrome Stable PPA and installing Chrome Stable..."
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
+
+## Setup RAMdisk for cache
+sudo mkdir -p /cache/chrome/
+sudo chmod 777 /cache/chrome/ -R
+echo "## Chrome cache in RAM" | sudo tee -a /etc/fstab
+echo "tmpfs    /cache/chrome    tmpfs    defaults,noatime,mode=1777      0       0" | sudo tee -a /etc/fstab
+sudo mount -a
+
+## Configure shortcut to use RAMdisk
+sed -i 's/Exec=\/opt\/google\/chrome\/google-chrome/Exec=\/opt\/google\/chrome\/google-chrome --disk-cache-dir="\/cache\/chrome\/" --disk-cache-size=600000000/g'  /usr/share/applications/google-chrome.desktop.test 
