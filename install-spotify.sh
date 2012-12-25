5523b6b364571eea56ee6ce5e04da0db14097e4f
kirtley <kirtley@kirtley-three>
2012-12-25 16:12:45 -0500

    modified:   install-spotify.sh

--- install-spotify.sh
+++ install-spotify.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 ## Kirtley Wienbroer
 ## kirtley@osgenie.com
-## http://www.webupd8.org/2011/12/how-to-install-native-spotify-linux.html
+## http://www.liberiangeek.net/2012/04/the-quickest-way-to-install-spotify-client-in-ubuntu-12-04-precise-pangolin/
+## http://askubuntu.com/questions/128026/is-there-a-spotify-repository-that-works-for-12-04
 
 echo "Adding Spotify Stable PPA and installing Spotify Stable..."
 sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list'
