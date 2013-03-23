80ae5c315c856e6ed19b9f41a9dd089f199a8966
kirtley <kirtley@kirtley-zbox>
2013-03-23 14:00:48 -0700

    updates

--- install-ownCloud-client.sh
+++ install-ownCloud-client.sh
@@ -1,7 +1,16 @@
 #/bin/bash
+# Kirtley Wienbroer
+# kirtley@osgenie.com
+# http://software.opensuse.org/download/package?project=isv:ownCloud:devel&package=owncloud-client
+server_distro=$(lsb_release  -is)
 server_release=$(lsb_release -rs)
-echo "deb http://download.opensuse.org/repositories/isv:ownCloud:devel/xUbuntu_$server_release/ /" | sudo tee /etc/apt/sources.list.d/owncloud-client.list
-wget http://download.opensuse.org/repositories/isv:ownCloud:devel/xUbuntu_$server_release/Release.key
-sudo apt-key add - < Release.key
-sudo apt-get update
-sudo apt-get install -y owncloud-client
+if [ "$server_distro" == "Ubuntu" ]; then
+    echo "deb http://download.opensuse.org/repositories/isv:ownCloud:devel/xUbuntu_$server_release/ /" | sudo tee /etc/apt/sources.list.d/owncloud-client.list
+    wget http://download.opensuse.org/repositories/isv:ownCloud:devel/xUbuntu_$server_release/Release.key
+    sudo apt-key add - < Release.key
+    sudo apt-get update
+    sudo apt-get install -y owncloud-client
+else
+    echo "Not a standard Ubuntu installation, you must install manually"
+    echo "see http://owncloud.org/sync-clients/ for instructions"
+fi
